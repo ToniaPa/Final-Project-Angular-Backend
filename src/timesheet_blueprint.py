aa472faa2a185a6123e8105b8c3409dd1c4bf659
Tonia Papadogiorgaki <papadogiorgaki@outlook.com>
2024-06-19 19:55:29 +0300

    changes in timesheet blueprint

--- src/timesheet_blueprint.py
+++ src/timesheet_blueprint.py
@@ -26,10 +26,16 @@ def add_timesheet():
 # @jwt_required()
 def get_timesheets_by_date(dateOfWork):
     try:
-        timesheet = Timesheet.objects(dateOfWork=dateOfWork).first()
-        if timesheet:
-            return Response(json.dumps(timesheet.to_mongo()), status=200)
-        return Response(json.dumps({"msg": "Timesheet for given date not found"}), status=404)
+        timesheets = Timesheet.objects(dateOfWork=dateOfWork).all()        
+        timesheets_list = [timesheet.to_mongo().to_dict() for timesheet in timesheets]
+        if (timesheets_list):     
+            # return Response(json.dumps(timesheets_list), status=200)
+             return Response(json.dumps(timesheets_list, default=str), status=200)   
+        return Response(json.dumps({"msg": "Collection Timesheets is empty"}), status=404)
+        # timesheet = Timesheet.objects(dateOfWork=dateOfWork).all()
+        # if timesheet:
+        #     return Response(json.dumps(timesheet.to_mongo()), status=200)
+        # return Response(json.dumps({"msg": "Timesheets for given date not found"}), status=404)
     except Exception as e:
         print(e)
         return Response(json.dumps({"msg": str(e)}), status=400)
@@ -58,21 +64,6 @@ def get_timesheets_by_client_afm(clientAfm):
         return Response(json.dumps({"msg": str(e)}), status=400)
 
 
-@timesheet.route("/afm/<string:afm>", methods=["PATCH"])
-def update_timesheet_by_id(id):
-    try:
-        data = request.get_json() 
-        # data = request.get_json # = problem for update
-        print(data)
-        Timesheet.objects(_id=id).update_one(**data)
-        return Response(json.dumps({"msg": "Worker updated"}), status=200)
-    except Exception as e:
-        print(e)
-        return Response(json.dumps({"msg": str(e)}), status=400)
-
-
-###
-
 @timesheet.route("/", methods=["GET"])
 # @jwt_required()
 def get_all_timesheets():    
@@ -81,21 +72,34 @@ def get_all_timesheets():
         timesheets = Timesheet.objects.all()        
         timesheets_list = [timesheet.to_mongo().to_dict() for timesheet in timesheets]
         if (timesheets_list):     
-            # return Response(json.dumps(workers_list), status=200)
+            # return Response(json.dumps(timesheets_list), status=200)
              return Response(json.dumps(timesheets_list, default=str), status=200)   
         return Response(json.dumps({"msg": "Collection Timesheets is empty"}), status=404)
     except Exception as e:
         print(e)
-        return Response(json.dumps({"msg": str(e)}), status=400)    
+        return Response(json.dumps({"msg": str(e)}), status=400)   
+    
+
+@timesheet.route("/id/<string:id>", methods=["PATCH"])
+def update_timesheet_by_id(id):
+    try:
+        data = request.get_json() 
+        # data = request.get_json # = problem for update
+        print(data)
+        Timesheet.objects(_id=ObjectId(id)).update_one(**data)
+        return Response(json.dumps({"msg": "Timesheet updated"}), status=200)
+    except Exception as e:
+        print(e)
+        return Response(json.dumps({"msg": str(e)}), status=400) 
 
 
 @timesheet.route("/id/<string:id>", methods=["DELETE"])
 # @jwt_required()
-def delete_timesheet(id):
+def delete_timesheet_by_id(id):
     try:
         # data = request.get_json() # = problem for delete!
         data = request.get_json        
-        Timesheet.objects(_id=id).delete()        
+        Timesheet.objects(_id=ObjectId(id)).delete()        
         return Response(json.dumps({"msg": "Timesheet deleted successfully!"}), status=200)
     except Exception as e:
         print(e)
